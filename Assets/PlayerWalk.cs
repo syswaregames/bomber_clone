8ea745387c3bb5a0f13c1167c405a893515a2fce
Leonardo da luz <leoluzprog@gmail.com>
2023-05-31 04:05:18 -0300

    walk funcionando mas nao da forma como eu gostaria

--- Assets/PlayerWalk.cs
+++ Assets/PlayerWalk.cs
@@ -12,14 +12,22 @@ public class PlayerWalk : MonoBehaviour
     [ReadOnly]
     public bool facingRight = true;
     [ReadOnly]
+    public WalkDirection walkDirection;
+    public enum WalkDirection { down, up, left, right }
+    [ReadOnly]
     public float currentXSpeed;
     [ReadOnly]
     public float currentYSpeed;
-
+    [ReadOnly]
+    public float targetXSpeed;
+    [ReadOnly]
+    public float targetYSpeed;
+    public LayerMask wallLayermask;
     [Header("Components")]
     public Animator animator;
     public Rigidbody2D rb;
     public SpriteRenderer spriteRenderer;
+    public Collider2D collider;
     // Start is called before the first frame update
     void Start()
     {
@@ -34,6 +42,7 @@ public class PlayerWalk : MonoBehaviour
 
     void FixedUpdate()
     {
+
         HandleWalk();
         setRbSpeedForAnimator();
     }
@@ -43,20 +52,63 @@ public class PlayerWalk : MonoBehaviour
         animator.SetFloat("xVelocity", rb.velocity.x);
         animator.SetFloat("yVelocity", rb.velocity.y);
     }
+
+    Vector2 deviateDirection(Vector2 testDirection)
+    {
+
+        var result = Physics2D.Raycast(collider.bounds.center, testDirection, 0.55f, wallLayermask);
+        Debug.DrawRay(collider.bounds.center, testDirection * 0.55f, Color.red);
+        if (result.collider is null)
+        {
+            var perpendicular = Vector2.Perpendicular(testDirection);
+            result = Physics2D.Raycast((Vector2)collider.bounds.center + (perpendicular * 0.2f), testDirection, 0.55f, wallLayermask);
+            Debug.DrawRay((Vector2)collider.bounds.center + (perpendicular * 0.2f), testDirection * 0.55f, Color.red);
+
+            if (result.collider is null)
+            {
+                perpendicular = perpendicular * -1;
+                result = Physics2D.Raycast((Vector2)collider.bounds.center + (perpendicular * 0.2f), testDirection, 0.55f, wallLayermask);
+                Debug.DrawRay((Vector2)collider.bounds.center + (perpendicular * 0.2f), testDirection * 0.55f, Color.red);
+            }
+        }
+        if (result.collider is not null)
+        {
+            if (result.normal * -1 == testDirection && (result.normal.x==0f || result.normal.y==0f))
+            {
+                return testDirection;
+            }
+
+            var perpendicular = Vector2.Perpendicular(result.normal);
+            print("Eita" + result.normal);
+            print("Perp" + Vector2.Perpendicular(result.normal));
+            Vector3 direcaoDesvio = Vector3.ProjectOnPlane(testDirection, result.normal).normalized;
+            print("Perp" + direcaoDesvio);
+            return direcaoDesvio;
+
+        }
+        else
+        {
+
+        }
+        return testDirection;
+    }
     void HandleWalk()
     {
-        var xAxis = Input.GetAxisRaw("Horizontal");
-        var yAxis = Input.GetAxisRaw("Vertical");
+        calculateWalkDirection();
+        var axis = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        axis = deviateDirection(axis);
 
-        var targetXSpeed = xAxis * maxSpeed;
-        var targetYSpeed = yAxis * maxSpeed;
-        currentXSpeed = GetNewSpeed(targetXSpeed, rb.velocity.x, acceleration, deceleration);
-        currentYSpeed = GetNewSpeed(targetYSpeed, rb.velocity.y, acceleration, deceleration);
-        if (xAxis > 0f)
-            print(currentXSpeed);
+        targetXSpeed = axis.x * maxSpeed;
+        targetYSpeed = axis.y * maxSpeed;
+
+
+
+        currentXSpeed = targetXSpeed;//GetNewSpeed(targetXSpeed, rb.velocity.x, acceleration, deceleration);
+        currentYSpeed = targetYSpeed; // GetNewSpeed(targetYSpeed, rb.velocity.y, acceleration, deceleration);
+
         rb.velocity = new Vector2(currentXSpeed, currentYSpeed);
 
-        if (xAxis != 0 || yAxis != 0)
+        if (targetXSpeed != 0 || targetYSpeed != 0)
         {
             animator.SetBool("walking", true);
         }
@@ -65,11 +117,63 @@ public class PlayerWalk : MonoBehaviour
             animator.SetBool("walking", false);
         }
 
-        if (xAxis > 0f)
+
+        //calculateWalkDirection();
+    }
+    
+    void calculateWalkDirection()
+    {
+
+        var x = rb.velocity.x;
+        var y = rb.velocity.y;
+        if (x == 0f && y == 0f)
+        {
+            return;
+        }
+        if (Mathf.Abs(x) == Mathf.Abs(y))
+        {
+            return;
+        }
+
+
+
+        WalkDirection newWalkDirection = walkDirection;
+        var isHorizontalGreater = Mathf.Abs(x) > Mathf.Abs(y);
+
+        if (isHorizontalGreater)
+        {
+            if (x > 0f)
+            {
+                newWalkDirection = WalkDirection.right;
+            }
+            else if (x < 0f)
+            {
+                newWalkDirection = WalkDirection.left;
+            }
+        }
+        else
+        {
+            if (y > 0f)
+            {
+                newWalkDirection = WalkDirection.up;
+            }
+            else if (y < 0f)
+            {
+                newWalkDirection = WalkDirection.down;
+            }
+        }
+
+        if (newWalkDirection != walkDirection)
+        {
+            animator.SetInteger("walkDirection", (int)newWalkDirection);
+        }
+        walkDirection = newWalkDirection;
+
+        if (x > 0f)
         {
             SetFacingToRight(true);
         }
-        else if (xAxis < 0f)
+        else if (x < 0f)
         {
             SetFacingToRight(false);
         }
@@ -115,4 +219,19 @@ public class PlayerWalk : MonoBehaviour
         }
         return currentSpeed;
     }
+
+    public void OnCollisionEnter2D(Collision2D col)
+    {
+        // print(rb.velocity);
+        // teste(col);
+
+    }
+    public void OnCollisionStay2D(Collision2D col)
+    {
+        // teste(col);
+        //Contornate(col);
+
+    }
+
+
 }
